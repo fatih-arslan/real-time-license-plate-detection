01d1fd83909aa9351de8cd19664e8340a422d2c9
Fatih Arslan <fatiharslan748@gmail.com>
2024-03-03 13:26:41 +0300

    initial commit

---
+++ YOLO_Video.py
@@ -0,0 +1,52 @@
+from ultralytics import YOLO
+import cv2
+import math
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Update with your Tesseract path
+
+def video_detection(path_x):
+    video_capture = path_x
+    #Create a Webcam Object
+    cap=cv2.VideoCapture(video_capture)
+    frame_width=int(cap.get(3))
+    frame_height=int(cap.get(4))
+    #out=cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P','G'), 10, (frame_width, frame_height))
+
+    model=YOLO("../best.pt")
+    classNames = ["plate"
+                  ]
+    while True:
+        success, img = cap.read()
+        results=model(img,stream=True)
+        for r in results:
+            boxes=r.boxes
+            for box in boxes:
+                x1,y1,x2,y2=box.xyxy[0]
+                x1,y1,x2,y2=int(x1), int(y1), int(x2), int(y2)
+
+                # Extract plate region
+                plate_region = img[y1:y2, x1:x2]
+
+                # Use OCR to extract text from the plate region
+                text = pytesseract.image_to_string(plate_region, config='--psm 8')
+
+                print(x1,y1,x2,y2)
+                cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255),3)
+                conf=math.ceil((box.conf[0]*100))/100
+                cls=int(box.cls[0])
+                class_name=classNames[cls]
+                label=f'{text}{conf}'
+                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                print(t_size)
+                c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                cv2.rectangle(img, (x1,y1), c2, [255,0,255], -1, cv2.LINE_AA)  # filled
+                cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
+
+        yield img
+        #out.write(img)
+        #cv2.imshow("image", img)
+        #if cv2.waitKey(1) & 0xFF==ord('1'):
+            #break
+    #out.release()
+cv2.destroyAllWindows()
